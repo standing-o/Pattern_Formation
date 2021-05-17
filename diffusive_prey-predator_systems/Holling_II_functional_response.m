ab88c56f80c43a097616ed99f8c34c490b4db032
OH-Seoyoung <eileen1426@naver.com>
2021-05-17 22:48:14 +0900

    20210517

--- diffusive_prey-predator_systems/Holling_II_functional_response.m
+++ diffusive_prey-predator_systems/Holling_II_functional_response.m
@@ -1,72 +1,59 @@
 clear; clc; close all;
 
-% spatial discretization
-xright = 10; nx = 50;
-yright = 10; ny = floor(nx*yright/xright);
-h = xright/nx;
-x = linspace(-0.5*h, xright + 0.5*h, nx+2)';
-y = linspace(-0.5*h, yright + 0.5*h, ny+2);
+% spatial and time discretization
+nx = 50; 
+x = linspace(0,nx-1,nx)'; 
+h = x(2) - x(1);  % space step
+dt = 5*10^(-3);  % time step
+r = dt/h^2;
+maxit = 2000;
 
 % parameters which govern equation
-alpha = 0.175;  % prey's density at which the predator has the maximum kill rate
-beta = 0.95;  % maximum birth
-gamma = 0.5;  % dead rate of the predator
-d = 20;  % diffusion rate of the predator
+alpha = 0.5;   % prey's density at which the predator has the maximum kill rate (0.175)
+beta = 1;   % maximum birth (0.95)
+gamma = 0.5;   % dead rate of the predator
+d = 20;   % diffusion rate of the predator
 eta = 0.8;  sigma = 0.1;
 ubar = 0.2; vbar = 0.1;
 
-
-% time discretization
-dt = 5*10^(-3);
-maxit = 10000;
-nn = maxit;
-
 for pit = 1:1
     rng(pit);
-    u = ubar + sigma*(2*rand(nx+2, ny+2)-1);  
-    v = vbar + sigma*(2*rand(nx+2, ny+2)-1);
+    
+    % initial condition
+    u = sigma*(2*rand(nx, maxit)-1);  
+    v = sigma*(2*rand(nx, maxit)-1);  
+    
+   % no-flux boundary condition 
+    u(1, :) = u(2, :);
+    u(end, :) = u(end-1, :);
+    v(1, :) = v(2, :);
+    v(end, :) = v(end-1, :); 
+        
     nu = u; nv = v;
     
     % numerical scheme
-    for it = 1:maxit
-        % no-flux boundary condition (in progress -> add y-direction )
-        u(1, :) = u(2, :);
-        u(end, :) = u(end-1, :);
-        u(:, 1) = u(:, 2);
-        u(:, end) = u(:, end-1);
-        v(1, :) = v(2, :);
-        v(end, :) = v(end-1, :);     
-        v(:, 1) = v(:, 2);
-        v(:, end) = v(:, end-1);
+    for it = 1:maxit   % time loop
+        for ix = 2:nx-1   % space loop
+            
+            % set the source terms
+            F = (u(ix, it).*v(ix, it))./(u(ix, it) + alpha);
+            f = u(ix, it).*(1-u(ix, it)) - F ;
+            g = -gamma*v(ix, it) + beta*F ;
+            
+            nu(ix, it) = u(ix, it) + r*(u(ix-1, it) + u(ix+1, it) - 2*u(ix, it)) + dt*f;
+            nv(ix, it) = v(ix, it) + r*d*(v(ix-1, it) + v(ix+1, it) - 2*v(ix, it)) + dt*g;
+        end
         
-        
-        
-        
-        
-        
-        % set the source terms
-        F = u(2:end-1, 2:end-1).*v(2:end-1, 2:end-1)./(u(2:end-1, 2:end-1) + alpha);
-        f = u(2:end-1, 2:end-1).*(1-u(2:end-1, 2:end-1)) - F ;
-        g = -gamma*v(2:end-1, 2:end-1) + beta*F ;
-        
-        % set the source terms
-        nu(2:end-1, 2:end-1) = u(2:end-1, 2:end-1) + dt*(lap(u,h) + f);
-        nv(2:end-1, 2:end-1) = v(2:end-1, 2:end-1) + dt*(d*lap(u,h) + g);
-       
         % reset the variables for next step
         u = nu;
         v = nv;
        
         % visualization
         if mod(it, 100) == 0
-            surf(x(2:end-1), y(2:end-1), v(2:end-1, 2:end-1)', 'linestyle', 'none');
-            axis image;
-            view(2);
-            set(gca, 'xtick', [], 'ytick', []);
+            imagesc(nu(:, it));
+%             set(gca, 'xtick', [], 'ytick', []);
             title([num2str(it)])
-            box on;
             colorbar;
-            shading interp;
             hold on;
             drawnow;
 %            print('-djpeg', sprintf('storage path', pit);
---
+++ diffusive_prey-predator_systems/test_2D.m
@@ -0,0 +1,76 @@
+clear; clc; close all;
+
+% spatial discretization
+xright = 10; nx = 50;
+yright = 10; ny = floor(nx*yright/xright);
+h = xright/nx;
+x = linspace(-0.5*h, xright + 0.5*h, nx+2)';
+y = linspace(-0.5*h, yright + 0.5*h, ny+2);
+
+% parameters which govern equation
+alpha = 0.175;  % prey's density at which the predator has the maximum kill rate
+beta = 0.95;  % maximum birth
+gamma = 0.5;  % dead rate of the predator
+d = 20;  % diffusion rate of the predator
+eta = 0.8;  sigma = 0.1;
+ubar = 0.2; vbar = 0.1;
+
+
+% time discretization
+dt = 10^(-10);
+maxit = 10000;
+nn = maxit;
+
+for pit = 1:1
+    rng(pit);
+    u = ubar + sigma*(2*rand(nx+2, ny+2)-1);  
+    v = vbar + sigma*(2*rand(nx+2, ny+2)-1);
+    nu = u; nv = v;
+    
+    % numerical scheme
+    for it = 1:maxit
+        % no-flux boundary condition (in progress -> add y-direction )
+        u(1, :) = u(2, :);
+        u(end, :) = u(end-1, :);
+        u(:, 1) = u(:, 2);
+        u(:, end) = u(:, end-1);
+        v(1, :) = v(2, :);
+        v(end, :) = v(end-1, :);     
+        v(:, 1) = v(:, 2);
+        v(:, end) = v(:, end-1);
+        
+        
+        
+        
+        
+        
+        % set the source terms
+        F = u(2:end-1, 2:end-1).*v(2:end-1, 2:end-1)./(u(2:end-1, 2:end-1) + alpha);
+        f = u(2:end-1, 2:end-1).*(1-u(2:end-1, 2:end-1)) - F ;
+        g = -gamma*v(2:end-1, 2:end-1) + beta*F ;
+        
+        % set the source terms
+        nu(2:end-1, 2:end-1) = u(2:end-1, 2:end-1) + dt*(lap(u,h) + f);
+        nv(2:end-1, 2:end-1) = v(2:end-1, 2:end-1) + dt*(d*lap(v,h) + g);
+       
+        % reset the variables for next step
+        u = nu;
+        v = nv;
+       
+        % visualization
+        if mod(it, 100) == 0
+            surf(x(2:end-1), y(2:end-1), v(2:end-1, 2:end-1)', 'linestyle', 'none');
+            axis image;
+            view(2);
+            set(gca, 'xtick', [], 'ytick', []);
+            title([num2str(it)])
+            box on;
+            colorbar;
+            shading interp;
+            hold on;
+            drawnow;
+%            print('-djpeg', sprintf('storage path', pit);
+        end
+    end
+ 
+end
