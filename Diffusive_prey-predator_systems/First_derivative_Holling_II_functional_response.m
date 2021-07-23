78b40cd79c1a12e4a9b4fbd96f66e370e1a24024
OH-Seoyoung <eileen1426@naver.com>
2021-07-23 17:50:24 +0900

    20210723

--- Diffusive_prey-predator_systems/First_derivative_Holling_II_functional_response.m
+++ Diffusive_prey-predator_systems/First_derivative_Holling_II_functional_response.m
@@ -7,59 +7,61 @@ clear; clc; close all;
 nx = 50; 
 h = 1;  % space step
 x = linspace(-0.5*h,50+0.5*h,nx+2);
-dt = 10^(-10);  % time step
+dt = 5*10^(-3);  % time step
 r = dt/h^2;
-maxit = 100000;
+maxit = 10^(5);
+
+t = linspace(0, maxit*dt, maxit+1);
 
 % parameters which govern equation
 alpha = 0.5;   % prey's density at which the predator has the maximum kill rate (0.175)
 beta = 1;   % maximum birth (0.95)
 gamma = 0.5;   % dead rate of the predator
-d = 20;   % diffusion rate of the predator
-eta = 0.8;  sigma = 0.1;
+d = 0.1;   % diffusion rate of the predator
+sigma = 0.1;
+
 ubar = 0.2; vbar = 0.1;
 
 for pit = 1:1
     rng(pit);
+
+    u = zeros(nx,maxit+1); v = zeros(nx,maxit+1);
     
     % initial condition
-    u = ubar + sigma*(2*rand(nx, maxit)-1);  
-    v = vbar + sigma*(2*rand(nx, maxit)-1);  
+    u(:,1) = ubar + sigma*(2*rand(nx, 1)-1);  
+    v(:,1) = vbar + sigma*(2*rand(nx, 1)-1);  
     
-   % no-flux boundary condition 
-    u(1, :) = u(2, :);
-    u(end, :) = u(end-1, :);
-    v(1, :) = v(2, :);
-    v(end, :) = v(end-1, :); 
-        
-    nu = u; nv = v;
+    nu = zeros(nx,1); nv = zeros(nx,1);
     
     % numerical scheme
-    for it = 1:maxit   % time loop
+    for it = 1:maxit  % time loop
+        
+        % no-flux boundary condition
+        u(1, it) = u(2, it);
+        u(end, it) = u(end-1, it);
+        v(1, it) = v(2, it);
+        v(end, it) = v(end-1, it);
+        
         for ix = 2:nx-1   % space loop
-            
+%             it
             % set the source terms
             F = (u(ix, it)*v(ix, it))/(u(ix, it) + alpha);
             f = u(ix, it)*(1-u(ix, it)) - F ;
             g = -gamma*v(ix, it) + beta*F ;
             
-            nu(ix, it) = u(ix, it) + r*(u(ix-1, it) + u(ix+1, it) - 2*u(ix, it)) + dt*f;
-            nv(ix, it) = v(ix, it) + r*d*(v(ix-1, it) + v(ix+1, it) - 2*v(ix, it)) + dt*g;
+            u(ix, it+1) = u(ix, it) + r*(u(ix-1, it) + u(ix+1, it) - 2*u(ix, it)) + dt*f;
+            v(ix, it+1) = v(ix, it) + r*d*(v(ix-1, it) + v(ix+1, it) - 2*v(ix, it)) + dt*g;
+            
         end
-        
-        % reset the variables for next step
-        u = nu;
-        v = nv;
        
         % visualization
-        if mod(it, 1000) == 0
-            imagesc(nu(:, it));
-%             set(gca, 'xtick', [], 'ytick', []);
+        if mod(it, 100) == 0
+            surf(t(2:it), x(2:end-1), u(:,2:it), 'linestyle','none');
+            view(2);
+            xlabel('time'), ylabel('X')
             title([num2str(it)])
             colorbar;
-            hold on;
             drawnow;
-%            print('-djpeg', sprintf('storage path', pit);
         end
     end
  
--- Diffusive_prey-predator_systems/Fractional_derivative_Holling_II_functional_response.m
+++ Diffusive_prey-predator_systems/Fractional_derivative_Holling_II_functional_response.m
@@ -4,20 +4,27 @@
 clear; clc; close all;
 
 % spatial and time discretization
-nx = 50; 
-h = 1;  % space step
-x = linspace(-0.5*h,50+0.5*h,nx+2);
-dt = 10^(-10);  % time step
-r = dt/h^2;
-maxit = 100000;
+nx=50; h=1;
+dt = 5*10^(-3);  % time step
+maxit = 10000;
+T = dt*maxit;
+
+x = linspace(-0.5*h,50+0.5*h,nx+2)';
+t = linspace(0, T, maxit+1);
 
 % parameters which govern equation
 alpha = 0.175;   % prey's density at which the predator has the maximum kill rate (0.175)
 beta = 0.95;   % maximum birth (0.95)
 gam = 0.5;   % dead rate of the predator
-d = 20;   % diffusion rate of the predator
-eta = 0.8;  sigma = 0.1;
+d = 20;   % diffusion rate of the predator (0.1)
+sigma = 0.1;
 ubar = 0.2; vbar = 0.1;
+
+eta = 0.8;  % fractional order
+ddt = dt^eta;
+r = eta/ddt - 2/h^2;
+
+uu = zeros(50,maxit/100);
 
 for pit = 1:1
     rng(pit);
@@ -43,23 +50,39 @@ for pit = 1:1
             f = u(ix, it)*(1-u(ix, it)) - F ;
             g = -gam*v(ix, it) + beta*F ;
             
-            nu(ix, it) = u(ix, it) + r*(u(ix-1, it) + u(ix+1, it) - 2*u(ix, it)) + dt*f;
-            nv(ix, it) = v(ix, it) + r*d*(v(ix-1, it) + v(ix+1, it) - 2*v(ix, it)) + dt*g;
+            % Grunwald-Letnikov coefficients
+            c = zeros(1,it); cu = 0; cv = 0;
+            if it>2
+                for j = 2:it-1
+                    c(j) = ((1-(1+eta)/j)^j)*(dt)^(-eta);
+                    cu = cu + c(j).*u(ix,it-j);
+                    cv = cv + c(j).*v(ix,it-j);
+                end
+            end
+
+            nu(ix, it) = ddt*(r*u(ix, it) + (1/h^2)*(u(ix-1, it) + u(ix+1, it)) - cu + f);
+            nv(ix, it) = ddt*(r*v(ix, it) + (d/h^2)*(v(ix-1, it) + v(ix+1, it)) - cv + g);
+            
         end
         
         % reset the variables for next step
         u = nu;
         v = nv;
-       
+        
+      
+%         if mod(it, 100)==0
+%             uu(:,it/100)=nu(:,it);
+%         end
+        
+        
         % visualization
-        if mod(it, 1000) == 0
-            imagesc(nu(:, it));
-%             set(gca, 'xtick', [], 'ytick', []);
-            title([num2str(it)])
+        if mod(it, 100) == 0
+            surf(t(2:end)', x(2:end-1), u,'linestyle','none');
+            xlabel('iterations'), ylabel('X')
+            view(2);
+            title([num2str(it)]);
             colorbar;
-            hold on;
             drawnow;
-%            print('-djpeg', sprintf('storage path', pit);
         end
     end
  
